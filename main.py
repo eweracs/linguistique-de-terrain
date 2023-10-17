e6db45b5998e91140a80dcb4faa034229a9d8649
Sebastian Carewe <51919489+eweracs@users.noreply.github.com>
2023-10-17 12:28:45 +0200

    Idk mostly move data to separate file

--- main.py
+++ main.py
@@ -1,133 +1,10 @@
-words = [
-    "maħamadu",
-    "initʃe",
-    "ikakɛnɛ",
-    "netɔgɔ",
-    "musomani",
-    "nedẽmuso",
-    "musomanju",
-    "ãu",
-    "ã",
-    "i",
-    "e",
-    "etɔgɔ",
-    "muso",
-    "musow",
-    "walãba",
-    "walãni",
-    "tʃɛmãni",
-    "nedeʧɛ",
-    "tʃɛmãnju",
-    "tʃɛ",
-    "namasa",
-    "pɔmu",
-    "mɔbili",
-    "ala",
-    "ɲɛnamaja",
-    "dunja",
-    "dʒi",
-    "aɾidʒinɛ",
-    "hakɛto",
-    "teɾi",
-    "teɾimuso",
-    "teɾitʃɛ",
-    "teɾtʃɛw",
-    "teɾimusou",
-    "finiti",
-    "musomanifiniti",
-    "faː",
-    "fa",
-    "so",
-    "so",
-    "kulu",
-    "jiɾi",
-    "dʒiɾi",
-    "sã",
-    "sã",
-    "balemakɛ",
-    "balematʃe",
-    "kaadũ",
-    "taːma",
-    "tagama",
-    "bagã",
-    "dō",
-    "plaʒida",
-    "mɔkɛ",
-    "fini",
-    "fini",
-    "finju",
-    "su",
-    "su",
-    "su",
-    "suu",
-    "bagãu",
-    "ba",
-    "mɔmuso",
-    "kasunɔgɔ",
-    "ziɾi",
-    "bamuso",
-    "faʧɛ",
-    "fakɛ",
-    "inisoɣoma",
-    "inisogoma",
-    "kalã",
-    "nekafinju",
-    "sodẽ",
-    "dʒiɾi",
-    "jiɾi",
-    "kaboli",
-    "kaːkalã",
-    "ekafinju",
-    "sɛbẽnikɛ̃lã",
-    "kãlimu",
-    "koko",
-    "gabugu",
-    "ɲɛ̃naʤɛ",
-    "ɲinɛ",
-    "kɔnɔ",
-    "so",
-    "nekapɔmu",
-    "nekapɔmu",
-    "kalãso",
-    "mpalã",
-    "nekampalãw",
-    "nekampalã",
-    "ajehakɛto",
-    "a",
-    "a",
-    "da",
-    "ko",
-    "sãnakalãso",
-    "sãnakalãsow",
-    "sã",
-    "kakɛafɛ",
-    "kakɛkalãfɛ",
-    "mbɛndɔgɔnife",
-    "sɛbɛ̃fŭɾa",
-    "ʤiɾibulu",
-    "kɔɾɔmuso",
-    "ʤifile",
-    "kuluː",
-    "ɲɛda",
-    "ɲɛkɛnɛ",
-    "sãnakalãsokadineje",
-    "sãnakalãkadineje",
-    "nekɔɾɔmusow",
-    "ɑ̃kafinju",
-    "mɔʧɛ",
-    "kalãdẽkagafe",
-    "dʒiflẽw",
-    "mbɛpatidũ",
-    "gafe",
-    "gafew",
-    "bilemã",
-    "mbɛnipɔmudu"
-]
+from data import words
 
 
 def calculate_combinations():
     # collect all existing letters in the words
-    # if a letter is followed by the "ː" character. So aː would be considered one single letter. The same applies to the "̃" character.
+    # if a letter is followed by the "ː" character. So aː would be considered one single letter. The same applies to
+    # the "̃" character.
     combining_characters = ["ː", "̃", "̆"]
     letters = []
     for word in words:
@@ -142,24 +19,19 @@ def calculate_combinations():
     print(letters)
 
     # for each letter, collect all existing combinations: which letter can precede it, which can follow it,
-    # based on the words. If a letter before is one of the combining characters, then it is considered as part of the
-    # letter. So the letter before will also be taken into account.
+    # based on the words. If the letter after the current letter is a combining character, it is ignored.
     combinations = {}
     for letter in letters:
         combinations[letter] = {"before": [], "after": []}
         for word in words:
             if letter in word:
                 if word.index(letter) > 0:
-                    if word[word.index(letter) - 1] in combining_characters:
-                        combinations[letter]["before"].append(word[word.index(letter) - 2] + word[word.index(letter) - 1])
-                    else:
+                    if word[word.index(letter) - 1] not in combinations[letter]["before"]:
                         combinations[letter]["before"].append(word[word.index(letter) - 1])
                 if word.index(letter) < len(word) - 1:
-                    if word[word.index(letter) + 1] in combining_characters:
-                        combinations[letter]["after"].append(word[word.index(letter) + 1] + word[word.index(letter) + 2])
-                    else:
-                        combinations[letter]["after"].append(word[word.index(letter) + 1])
-
+                    if word[word.index(letter) + 1] not in combining_characters:
+                        if word[word.index(letter) + 1] not in combinations[letter]["after"]:
+                            combinations[letter]["after"].append(word[word.index(letter) + 1])
 
     # transform the combinations into an xlsx file.
     # For each letter, create a sheet with the letters that can precede it and the letters that can follow it
